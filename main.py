320e6e4d7190dc0b8382481e7c8a7c617c30c210
Cameron Hendey <cameronhendey246@gmail.com>
2026-02-23 13:12:38 +0200

    Adding new main file

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price"
+params = {"ids": "bitcoin", "vs_currencies": "usd"}
+
+r = requests.get(url, params=params, timeout=10)
+r.raise_for_status()
+data = r.json()
+
+print(data["bitcoin"]["usd"])
