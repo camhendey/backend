9d4ad1b6e856ad1273c08de56bf1b1fcee88357f
Cameron Hendey <cameronhendey246@gmail.com>
2026-02-23 14:25:01 +0200

    Update currency parameter in API request from USD to CAD

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 
 url = "https://api.coingecko.com/api/v3/simple/price"
-params = {"ids": "bitcoin", "vs_currencies": "usd"}
+params = {"ids": "bitcoin", "vs_currencies": "cad"}
 
 r = requests.get(url, params=params, timeout=10)
 r.raise_for_status()
 data = r.json()
 
-print(data["bitcoin"]["usd"])
+print(data["bitcoin"]["cad"])
